6e32b1f50c7b24e987b100f1e864c881103babe8
XuanThaiNguyen <42576329+XuanThaiNguyen@users.noreply.github.com>
2022-02-07 10:43:53 +0700

    Feature/create user manager (#3)
    * split component
    
    * add icons in actions

---
+++ frontend/src/modules/users/components/EnhanceTableHead/EnhanceTableHead.tsx
@@ -0,0 +1,40 @@
+import Checkbox from '@mui/material/Checkbox';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
+import { headCells } from '../../helpers/users.helper';
+import { EnhancedTableProps } from '../../types/users.type';
+
+const EnhanceTableHead = (props: EnhancedTableProps) => {
+  const { onSelectAllClick, numSelected, rowCount } = props;
+
+  return (
+    <TableHead>
+      <TableRow>
+        <TableCell padding="checkbox">
+          <Checkbox
+            color="primary"
+            indeterminate={numSelected > 0 && numSelected < rowCount}
+            checked={rowCount > 0 && numSelected === rowCount}
+            onChange={onSelectAllClick}
+            inputProps={{
+              'aria-label': 'select all desserts',
+            }}
+          />
+        </TableCell>
+        {headCells.map(headCell => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+          >
+            {headCell.label}
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+};
+
+export default EnhanceTableHead;
